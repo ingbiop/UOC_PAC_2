8eac5602c472872931a1c3d89f551b607642a23a
ingbiop <inpamcat@gmail.com>
2025-12-20 00:05:20 +0100

    actual

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 
-var fruits = ['Banana','Mango','Apple','Lemon'];
-console.log(fruits);
-console.log(fruits.length);
-for (var i=0; i < fruits.length; i++) {
-    console.log ('for: '+i+' : '+fruits[i]);
-}
 
