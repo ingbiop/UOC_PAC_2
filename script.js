d812b2e3aaee056d5c3e6eacaec5d4ed10455173
ingbiop <inpamcat@gmail.com>
2025-12-19 20:21:20 +0100

    actualitzacio 3

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-@charset "utf-8";
+
+var fruits = ['Banana','Mango','Apple','Lemon'];
+console.log(fruits);
+console.log(fruits.length);
+for (var i=0; i < fruits.length; i++) {
+    console.log ('for: '+i+' : '+fruits[i]);
+}
 
