581612bbb926e66b6b08b15c8f9728d518b995c5
ingbiop <inpamcat@gmail.com>
2025-12-22 12:47:57 +0100

    update

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
+const cards = document.querySelectorAll('.cartes-pokemon > div')
+console.log(cards)
+
+const filters = document.querySelectorAll('input[type="checkbox"]');
+console.dir(filters);
+
+filters.forEach(filter => {
+  filter.addEventListener('change', (e) => {
+    // console.log(e.target.name);
+    // console.log(e.target.checked);
+    const selected = e.target.name;
+
+    if (e.target.checked == false) {
+      cards.forEach(card => {
+        card.style.display = 'block'
+      })
+      return
+    }
+
+    cards.forEach(card => {
+      card.style.display = 'none'
+      const tipus = Array.from(card.querySelectorAll('p > span'))
+      if (tipus.some(classePokemon => classePokemon.classList.contains(selected))) {
+        card.style.display = 'block'
+      }
+    })
+  })
+})
+
+const search = document.querySelector('input[type="search"]')
+const cardsArray = Array.from(cards);
+
+search.addEventListener('input', (e) => {
+  // console.log(e.target.value)
+  const search = e.target.value.toLocaleLowerCase();
+  const results = cardsArray.filter(card => {
+    const name = card.querySelector('h2').textContent.toLocaleLowerCase()
+    return name.includes(search)
+  })
+  cards.forEach(card => card.style.display = 'none')
+  results.forEach(card => card.style.display = 'block')
+})
 
 
